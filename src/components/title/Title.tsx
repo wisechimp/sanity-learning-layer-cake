805a7e2f24c47f854e4725e75855a6a888a72aca
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-01-23 12:59:19 +0100

    feat: Create a Title component

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+
+const Title = (props: PropsWithChildren) => {
+  return (
+    <h1 className="text-2xl md:text-4xl lg:text-6xl font-semibold text-slate-800 text-pretty max-w-3xl">
+      {props.children}
+    </h1>
+  );
+};
+
+export default Title;
