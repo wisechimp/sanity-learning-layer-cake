49e71b2a113400ee69bde63fae7a0d645a0fc23a
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-01-23 13:31:33 +0100

    fix: Removing unused imports from files

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,11 +1,6 @@
 import Post from "@/components/post/Post";
-import { urlFor } from "@/sanity/lib/image";
 import { sanityFetch } from "@/sanity/lib/live";
 import { singlePostQuery } from "@/sanity/lib/queries";
-import components from "@/sanity/portableTextComponents";
-import { PortableText } from "next-sanity";
-import Image from "next/image";
-import Link from "next/link";
 import { notFound } from "next/navigation";
 
 type PostPageProps = {
