e405d20d1692a365e37c8a37fa18b9ce1b07e7b3
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-01-23 13:01:03 +0100

    feat: Adding the newly created components to the site

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Post from "@/components/post/Post";
 import { urlFor } from "@/sanity/lib/image";
 import { sanityFetch } from "@/sanity/lib/live";
 import { singlePostQuery } from "@/sanity/lib/queries";
@@ -23,28 +24,7 @@ const PostPage = async ({ params }: PostPageProps) => {
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      {post?.mainImage ? (
-        <Image
-          className="w-full aspect-[800/300]"
-          src={urlFor(post.mainImage)
-            .width(800)
-            .height(300)
-            .quality(80)
-            .auto("format")
-            .url()}
-          alt={post?.mainImage?.alt || ""}
-          width="800"
-          height="300"
-        />
-      ) : null}
-      <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
-      {post?.body ? (
-        <div className="prose">
-          <PortableText value={post.body} components={components} />
-        </div>
-      ) : null}
-      <hr />
-      <Link href="/posts">&larr; Return to index</Link>
+      <Post {...post} />
     </main>
   );
 };
--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,26 +1,19 @@
+import PostCard from "@/components/post-card/PostCard";
+import Title from "@/components/title/Title";
 import { sanityFetch } from "@/sanity/lib/live";
 import { allPostsQuery } from "@/sanity/lib/queries";
-import Link from "next/link";
 
 const PostsPage = async () => {
   const { data: posts } = await sanityFetch({ query: allPostsQuery });
 
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <h1 className="text-4xl font-bold">Post index</h1>
-      <ul className="grid grid-cols-1 divide-y divide-blue-500">
+      <Title>Post Index</Title>
+      <div className="flex flex-col gap-24 py-12">
         {posts.map((post) => (
-          <li key={post._id}>
-            <Link
-              className="block p-4 hover:text-blue-500"
-              href={`/posts/${post?.slug?.current}`}
-            >
-              {post?.title}
-            </Link>
-          </li>
+          <PostCard key={post._id} {...post} />
         ))}
-      </ul>
-      <Link href="/">&larr; Return home</Link>
+      </div>
     </main>
   );
 };
